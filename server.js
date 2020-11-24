c820766b48fa84e50cc88744d518f7b7727b49e9
proze <philippe.roze@sonovisiongroup.com>
2020-11-24 10:59:19 +0100

    Express

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var http = require('http');
-http.createServer(function (req, res) {
-    res.write('Hello word');
-    res.end();
+const server = http.createServer(function (req, res) {
+    const url = req.url;
+    if (url === '/about') {
+        res.write('<h1>about us page</h1>');
+        res.end();
+    } else if (url === '/contact') {
+        res.write('<h1>contact us page</h1>');
+        res.end();
+    } else {
+        res.write('<h1>unknowed page</h1>');
+        res.end();   
+    }
 }).listen(8080);
